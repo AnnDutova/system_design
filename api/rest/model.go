f42544fc0f2d82d158f9ce29bacfc6c521994c3a
a.dutova <a.dutova@yadro.com>
2026-01-09 21:52:24 +0300

    TASK-3: first view to db scheme

--- api/rest/model.go
+++ api/rest/model.go
@@ -8,60 +8,78 @@ import (
 
 type (
 	User struct {
-		ID           types.UUID `json:"id"`
-		Name         string     `json:"name" validate:"required"`
-		Email        string     `json:"email" validate:"required,email"`
-		Password     []byte     `json:"password" validate:"required,password"`
-		Created      time.Time  `json:"created"`
-		LastModified string     `json:"last_modified"`
+		ID       types.UUID `json:"id"`
+		Name     string     `json:"name" validate:"required"`
+		Email    string     `json:"email" validate:"required,email"`
+		Password []byte     `json:"password" validate:"required,password"`
+
+		Created      time.Time `json:"created"`
+		LastModified string    `json:"last_modified"`
 	}
 
 	Subscription struct {
 		AuthorID types.UUID `json:"author_id"`
 		UserID   types.UUID `json:"user_id"`
+
+		Created      time.Time `json:"created"`
+		LastModified string    `json:"last_modified"`
 	}
 
 	Post struct {
-		ID           types.UUID `json:"id"`
-		Text         string     `json:"text"`
-		UserID       types.UUID `json:"user_id"`
-		LocationID   types.UUID `json:"location_id"`
-		Created      time.Time  `json:"created"`
-		LastModified string     `json:"last_modified"`
-		Comments     []Comment  `json:"comments,omitempty"`
-		Media        []Media    `json:"media,omitempty"`
+		ID          types.UUID `json:"id"`
+		Description string     `json:"description"`
+		UserID      types.UUID `json:"user_id"`
+		LocationID  types.UUID `json:"location_id"`
+
+		Created      time.Time `json:"created"`
+		LastModified string    `json:"last_modified"`
+
+		Comments []Comment `json:"comments,omitempty"`
+		Media    []Media   `json:"media,omitempty"`
+		Rating   Rating    `json:"rating,omitempty"`
 	}
 
 	Media struct {
+		ID types.UUID `json:"id"`
+
 		Type  string `json:"type"`
+		Name  string `json:"file_name"`
 		Photo []byte `json:"photo"`
+
+		Created      time.Time `json:"created"`
+		LastModified string    `json:"last_modified"`
 	}
 
 	Comment struct {
-		ID           types.UUID `json:"id"`
-		UserID       types.UUID `json:"user_id"`
-		Text         string     `json:"text"`
-		Created      time.Time  `json:"created"`
-		LastModified string     `json:"last_modified"`
+		ID     types.UUID `json:"id"`
+		PostID types.UUID `json:"post_id"`
+		UserID types.UUID `json:"user_id"`
+		Text   string     `json:"text"`
+
+		Created      time.Time `json:"created"`
+		LastModified string    `json:"last_modified"`
 	}
 
 	Location struct {
-		ID           types.UUID `json:"id"`
-		Name         string     `json:"name" validate:"required" example:"Hamunaptra,Egypt"`
-		Created      time.Time  `json:"created"`
-		LastModified string     `json:"last_modified"`
+		ID   types.UUID `json:"id"`
+		Name string     `json:"name" validate:"required" example:"Hamunaptra,Egypt"`
+
+		Created      time.Time `json:"created"`
+		LastModified string    `json:"last_modified"`
 	}
 
 	Rating struct {
-		Points       uint8     `json:"point" validate:"required"`
+		Points uint8 `json:"point" validate:"required"`
+
 		Created      time.Time `json:"created"`
 		LastModified string    `json:"last_modified"`
 	}
 
 	Subscribe struct {
-		ID       types.UUID `json:"id"`
-		AuthorID types.UUID `json:"author_id" validate:"required"`
-		UserID   types.UUID `json:"user_id" validate:"required"`
+		AuthorID     types.UUID `json:"author_id" validate:"required"`
+		UserID       types.UUID `json:"user_id" validate:"required"`
+		Created      time.Time  `json:"created"`
+		LastModified string     `json:"last_modified"`
 	}
 
 	RestError struct {
